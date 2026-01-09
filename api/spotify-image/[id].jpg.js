18c81c77fdb664d4c7feba3e8787a5e1d553395b
EbisawaHaruki <k248002@kccollege.ac.jp>
2026-01-09 11:29:36 +0900

    Create [id].jpg.js

---
+++ api/spotify-image/[id].jpg.js
@@ -0,0 +1,26 @@
+export default async function handler(req, res) {
+  const { id } = req.query;
+
+  if (!id) {
+    res.status(400).send("id is required");
+    return;
+  }
+
+  // id = "ab67616d0000....jpg"
+  const imageId = id.replace(".jpg", "");
+  const spotifyImageUrl = `https://i.scdn.co/image/${imageId}`;
+
+  const response = await fetch(spotifyImageUrl);
+
+  if (!response.ok) {
+    res.status(404).send("Image not found");
+    return;
+  }
+
+  const arrayBuffer = await response.arrayBuffer();
+  const buffer = Buffer.from(arrayBuffer);
+
+  res.setHeader("Content-Type", "image/jpeg");
+  res.setHeader("Cache-Control", "public, max-age=86400");
+  res.send(buffer);
+}
